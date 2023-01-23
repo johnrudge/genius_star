7248349cafaa06a4c0d8b1e0bc4347d3cd700947
John Rudge <jfr23@cam.ac.uk>
2023-01-23 21:21:06 +0000

    Count all solutions for all blocker positions.

--- solve_all.py
+++ solve_all.py
@@ -1,5 +1,7 @@
-from genius_star import Dice, Game
+from itertools import combinations
 import numpy as np
+from genius_star import Dice, Game
+import exact_cover as ec
 
 
 def naive_solution_count():
@@ -20,38 +22,82 @@ def naive_solution_count():
     print(f" {i+1} games solved ({star_games} star games)")
 
 
-def solution_count():
+def count_dice_roll_solutions():
     dice = Dice()
-    game = Game()
+    solution_count(dice.all_rolls())
 
-    # Only solve problems which are related by symmetry once
-    rolls = np.array([game.board.equivalent_roll(r) for r in dice.all_rolls()])
-    unique_rolls, counts = np.unique(rolls, axis=0, return_counts=True)
+
+def count_all_solutions():
+    rolls = combinations(range(1, 49), 7)
+    solution_count(rolls)
+
+
+def solution_count(rolls):
+    game = Game()
 
     star_games = 0
     solved_games = 0
+    nosolution_games = 0
     unique_star_games = 0
     unique_solved_games = 0
+    unique_nosolution_games = 0
 
-    for i in range(unique_rolls.shape[0]):
-        roll = unique_rolls[i, :]
+    star_seen = set()
+    nostar_seen = set()
+    nosolution_seen = set()
+
+    for r in rolls:
+        roll = tuple(game.board.equivalent_roll(r))
+
+        # Only solve problems which are related by symmetry once
+        if roll in star_seen:
+            star_games += 1
+            solved_games += 1
+            continue
+        if roll in nostar_seen:
+            solved_games += 1
+            continue
+        if roll in nosolution_seen:
+            nosolution_games += 1
+            continue
+
         game.new_roll(roll, star=True)
+        try:
+            solution = game.solve()
+        except ec.error.NoSolution:
+            unique_nosolution_games += 1
+            nosolution_games += 1
+            nosolution_seen.add(roll)
+            continue
 
-        solution = game.solve()
-        solved_games += counts[i]
+        solved_games += 1
         unique_solved_games += 1
 
         if solution.game.star == True:
-            star_games += counts[i]
+            star_games += 1
             unique_star_games += 1
+            star_seen.add(roll)
+        else:
+            nostar_seen.add(roll)
 
-        print(f" {solved_games} games solved ({star_games} star games)", end="\r")
+        print(
+            f" {unique_solved_games} unique games solved ({unique_star_games} unique star games)",
+            end="\r",
+        )
 
-    print(f" {solved_games} games solved ({star_games} star games)")
     print(
         f" {unique_solved_games} unique games solved ({unique_star_games} unique star games)"
     )
+    print(f" {solved_games} games solved ({star_games} star games)")
+
+    if nosolution_games > 0:
+        print(
+            f"Searched {solved_games + nosolution_games} games ({unique_solved_games+unique_nosolution_games} unique)"
+        )
 
 
 if __name__ == "__main__":
-    solution_count()
+    print("Counting dice roll solutions:")
+    count_dice_roll_solutions()
+    print("Counting solutions for all blocker positions (takes a long time!):")
+    count_all_solutions()
